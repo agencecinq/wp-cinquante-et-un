ad850f5934dfbfa4b8b422028d22ef5666713091
Jérémy Levron <19h47@users.noreply.github.com>
2026-06-29 12:28:24 +0200

    :fire: Remove Weglot custom language selector helper

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,7 +9,6 @@ load('cinq-media-text-reveal', () => import('./components/MediaTextReveal.ts'));
 load('cinq-child-pages-by-parent', () => import('./components/ChildPagesByParent.ts'));
 load('cinq-slideshow', () => import('./components/Slideshow.ts'));
 load('cinq-mobile-menu', () => import('./components/MobileMenu.ts'));
-load('cinq-select-redirect', () => import('./components/SelectRedirect.ts'));
 
 // The object cinq is defined in includes/Setup/Enqueue.php and localized to app.js
 // It provideds useful information such as the text domain, template_directory_uri, base_url, etc.
